94381ee651b285352c65dfc84e616d024013c1da
Iremiwa <somoayo@yahoo.com>
2020-03-16 01:36:11 +0100

    Added Updates

--- server.js
+++ server.js
@@ -1,25 +1,28 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const path = require("path");
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
-
-app.use(express.json);
-app.use(express.static(path.join(__dirname, "client", "build")));
-
-
-
- require("dotenv").config();
- const url = process.env.ATLAS_URI;
+require("dotenv").config();
+const url = process.env.ATLAS_URI;
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
-    () => {
-      console.log("MongoDB database connection established");
-    }
-  )
-  .catch(err => console.log(err));
+   () => {
+     console.log("MongoDB database connection established");
+   }
+ )
+ .catch(err => console.log(err));
 
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "client", "build")));
 
     app.get("*", (req, res) => {
       res.sendFile(path.join(__dirname, "client", "build", "index.html"));
@@ -30,6 +33,6 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useCrea
   }
 
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}!`));
